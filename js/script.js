99729336d1cc11f7524238db26f84c19f82e655c
Yasmin <ydossantoscorporate@gmail.com>
2026-05-12 22:45:11 -0300

    feat : iniciando javaScript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const pessoa = {
+    nome: "Yasmin",
+    idade: 20,
+    cidade: "São Paulo",
+    casado: false
+}
+
+console.log(pessoa.nome + " tem " + pessoa.idade + " anos, mora em " + pessoa.cidade + " e é casado? " + pessoa.casado);
